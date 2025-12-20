a16c49a40ae2cdc628b28e417432f3d7bafa10aa
Velocitcs <ribnik625@gmail.com>
2025-12-20 18:34:47 +0000

    New Switch redesign

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -20,6 +20,9 @@ import "./Switch.css";
 
 import { classNameFactory } from "@api/Styles";
 import { classes } from "@utils/misc";
+import { BaseSwitch } from "@webpack/common";
+import { ReactNode } from "react";
+import { handleThemeComponent } from "./handleThemeComponent";
 
 const switchCls = classNameFactory("vc-switch-");
 
@@ -30,47 +33,52 @@ export interface SwitchProps {
     disabled?: boolean;
     checked: boolean;
     onChange: (checked: boolean) => void;
+    title?: ReactNode;
+    description?: ReactNode;
 }
 
-export function Switch({ checked, onChange, disabled }: SwitchProps) {
-    return (
-        <div>
-            <div className={classes(switchCls("container"), "default-colors", switchCls({ checked, disabled }))}>
-                <svg
-                    className={switchCls("slider")}
-                    viewBox="0 0 28 20"
-                    preserveAspectRatio="xMinYMid meet"
-                    aria-hidden="true"
-                    style={{
-                        transform: checked ? "translateX(12px)" : "translateX(-3px)",
-                    }}
-                >
-                    <rect fill="white" x="4" y="0" height="20" width="20" rx="10" />
-                    <svg viewBox="0 0 20 20" fill="none">
-                        {checked ? (
-                            <>
-                                <path fill={SWITCH_ON} d="M7.89561 14.8538L6.30462 13.2629L14.3099 5.25755L15.9009 6.84854L7.89561 14.8538Z" />
-                                <path fill={SWITCH_ON} d="M4.08643 11.0903L5.67742 9.49929L9.4485 13.2704L7.85751 14.8614L4.08643 11.0903Z" />
-                            </>
-                        ) : (
-                            <>
-                                <path fill={SWITCH_OFF} d="M5.13231 6.72963L6.7233 5.13864L14.855 13.2704L13.264 14.8614L5.13231 6.72963Z" />
-                                <path fill={SWITCH_OFF} d="M13.2704 5.13864L14.8614 6.72963L6.72963 14.8614L5.13864 13.2704L13.2704 5.13864Z" />
-                            </>
-                        )}
-
-                    </svg>
+const SwitchComponent = ({ checked, onChange, disabled }: SwitchProps) => (
+    <div>
+        <div className={classes(switchCls("container"), "default-colors", switchCls({ checked, disabled }))}>
+            <svg
+                className={switchCls("slider")}
+                viewBox="0 0 28 20"
+                preserveAspectRatio="xMinYMid meet"
+                aria-hidden="true"
+                style={{
+                    transform: checked ? "translateX(12px)" : "translateX(-3px)",
+                }}
+            >
+                <rect fill="white" x="4" y="0" height="20" width="20" rx="10" />
+                <svg viewBox="0 0 20 20" fill="none">
+                    {checked ? (
+                        <>
+                            <path fill={SWITCH_ON} d="M7.89561 14.8538L6.30462 13.2629L14.3099 5.25755L15.9009 6.84854L7.89561 14.8538Z" />
+                            <path fill={SWITCH_ON} d="M4.08643 11.0903L5.67742 9.49929L9.4485 13.2704L7.85751 14.8614L4.08643 11.0903Z" />
+                        </>
+                    ) : (
+                        <>
+                            <path fill={SWITCH_OFF} d="M5.13231 6.72963L6.7233 5.13864L14.855 13.2704L13.264 14.8614L5.13231 6.72963Z" />
+                            <path fill={SWITCH_OFF} d="M13.2704 5.13864L14.8614 6.72963L6.72963 14.8614L5.13864 13.2704L13.2704 5.13864Z" />
+                        </>
+                    )}
                 </svg>
-                <input
-                    disabled={disabled}
-                    type="checkbox"
-                    className={switchCls("input")}
-                    tabIndex={0}
-                    checked={checked}
-                    onChange={e => onChange(e.currentTarget.checked)}
-                    aria-label="toggleSwich"
-                />
-            </div>
+            </svg>
+            <input
+                disabled={disabled}
+                type="checkbox"
+                className={switchCls("input")}
+                tabIndex={0}
+                checked={checked}
+                onChange={e => onChange(e.currentTarget.checked)}
+                aria-label="toggleSwitch"
+            />
         </div>
-    );
+    </div>
+);
+
+export function Switch({ checked, onChange, disabled, title, description }: SwitchProps) {
+    const baseSwitch = <BaseSwitch label={title} description={description} hasIcon={true} checked={checked} onChange={onChange} disabled={disabled} />;
+
+    return handleThemeComponent({ render: baseSwitch, setting: "switchRedesign" }) ?? <SwitchComponent checked={checked} onChange={onChange} disabled={disabled} />;
 }
